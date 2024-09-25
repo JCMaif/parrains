aebb3621798afd70edf7c66b598d9733716291a5
Jean-Charles Audebert <jean-charles.audebert@maif.fr>
2024-09-25 09:02:03 +0200

    Initialisation Front et Back

---
+++ src/main/java/com/simplon/parrains/ParrainsApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.parrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplon/parrains/ParrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.parrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
