d175a2e3b996c972a9453fac31944db11b424e2a
CADET Flavien <flavien.cadet@externe.maif.fr>
2024-11-04 13:52:12 +0100

    add shouldReturnUtilisateurById test ok

--- src/main/java/com/simplon/parrains/model/dto/UtilisateurDto.java
+++ src/main/java/com/simplon/parrains/model/dto/UtilisateurDto.java
@@ -1,6 +1,7 @@
 package com.simplon.parrains.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.simplon.parrains.model.Utilisateur;
 import com.simplon.parrains.model.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
--- src/test/java/com/simplon/parrains/service/UtilisateurServiceTest.java
+++ src/test/java/com/simplon/parrains/service/UtilisateurServiceTest.java
@@ -6,6 +6,8 @@ import com.simplon.parrains.model.Utilisateur;
 import com.simplon.parrains.model.dto.UtilisateurDto;
 import com.simplon.parrains.model.enums.Role;
 import com.simplon.parrains.repository.UtilisateurRepository;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,6 +15,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -27,8 +30,9 @@ class UtilisateurServiceTest {
     private UtilisateurService utilisateurService;
 
     @Test
+    @DisplayName("Retourner tous les utilisateurs :")
     void shouldReturnAllUtilisateurs() {
-      
+      //Arrange
         Utilisateur porteur = new Porteur();
         porteur.setUsername("porteur1");
         porteur.setFirstname("John");
@@ -47,12 +51,40 @@ class UtilisateurServiceTest {
 
         when(utilisateurRepository.findAll()).thenReturn(List.of(porteur, parrain));
 
+        //Act
         List<UtilisateurDto> result = utilisateurService.getAllUtilisateurs();
 
+        //Assert
         assertThat(result).hasSize(2);
         assertThat(result.get(0).getUsername()).isEqualTo("porteur1");
         assertThat(result.get(1).getUsername()).isEqualTo("parrain1");
 
         Mockito.verify(utilisateurRepository).findAll();
     }
+
+    @Test
+    @DisplayName("Retourner un utilisateur par son id :")
+    void shouldReturnUtilisateurById() {
+        // Arrange
+        Utilisateur porteur = new Porteur();
+        porteur.setId(1L); 
+        porteur.setUsername("porteur1");
+        porteur.setFirstname("John");
+        porteur.setLastname("Doe");
+        porteur.setCompany("Startup Inc.");
+        porteur.setEmail("porteur1@example.com");
+        porteur.setRole(Role.PORTEUR);
+        porteur.setDeleted(false);
+        
+        when(utilisateurRepository.findById(1L)).thenReturn(Optional.of(porteur));
+
+        // Act
+        Optional<UtilisateurDto> foundUser = utilisateurService.getUtilisateurById(1L);
+
+        // Assert
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get()).usingRecursiveComparison().isEqualTo(porteur);
+    }
+        
+
 }
